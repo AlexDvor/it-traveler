8c95a9bde34ff365df568595894ce996419c601b
Alex <alexmatvichuk@gmail.com>
2024-11-25 12:10:25 +0100

    Add HomePage

--- src/components/IButton/IButton.vue
+++ src/components/IButton/IButton.vue
@@ -1,5 +1,5 @@
 <template>
   <button class="py-3 px-10 bg-orange rounded-xl text-white font-bold tracking-wider">
-    Start to work
+    <slot>Click me</slot>
   </button>
 </template>
---
+++ src/views/HomePageView.vue
@@ -0,0 +1,22 @@
+<script setup>
+import IButton from '../components/IButton/IButton.vue'
+</script>
+
+<template>
+  <main class="h-screen grid grid-cols-2 grid-rows-1 bg-primary">
+    <section class="flex justify-center items-center p-10">
+      <div class="text-white text-center">
+        <img class="inline mb-6" src="../assets/icons/map-pin.svg" />
+        <h1 class="font-bold text-4xl mb-7">IT traveler</h1>
+        <p class="leading-6 mb-11">
+          A simple and user-friendly web application that allows you to mark your favorite places as
+          well as those you'd love to visit. So don't wait - give it a try!
+        </p>
+        <IButton>Start to work</IButton>
+      </div>
+    </section>
+    <section>
+      <img class="h-full object-cover w-full" src="../assets/images/static-map.png" alt="map" />
+    </section>
+  </main>
+</template>

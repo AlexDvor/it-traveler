5e8dc79bb582119b41502532259fd8ee8015e674
AlexDvor <alexmatvichuk@gmail.com>
2025-07-22 12:44:44 +0200

    Fix Greeting Text

--- src/views/GreetingView.vue
+++ src/views/GreetingView.vue
@@ -9,8 +9,8 @@ import BaseLayout from '../layouts/BaseLayout.vue'
       <img class="inline mb-6" src="../assets/icons/map-pin.svg" alt="" />
       <h1 class="font-bold text-4xl mb-7">IT traveler</h1>
       <p class="leading-6 mb-11">
-        Простий і зручний веб додаток, який дозволить тобі відмічати твої улюблені місця, а також
-        ті, в яких би ти дуже хотів побувати. Тож не зволікай і спробуй сам.
+        A simple and convenient web app that lets you mark your favorite places, as well as those
+        you’d love to visit. So don’t wait—give it a try!.
       </p>
       <IButton to="/auth/registration">Start to Work</IButton>
     </div>
